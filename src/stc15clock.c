8d413b5406be216d08028fdb5f0f253c22cfa2fc
Jakub Sokół <beamer.kun@gmail.com>
2016-12-17 23:15:13 +0100

    Adjust LED brightness based on light level

--- src/stc15clock.c
+++ src/stc15clock.c
@@ -3,6 +3,7 @@
 #include "ds1302.h"
 #include "stc15_buttons.h"
 #include "stc15_display.h"
+#include "stc15_dusk.h"
 
 void reset_io() {
   P2 = 0xFF;
@@ -18,15 +19,32 @@ void reset_io() {
   P3M1 &= 0xC3;
 }
 
+void check_low_light() {
+  stc15_update_light_state();
+  if(stc15_is_low_light()) {
+    P2M0 = 0x00;
+    P2M1 = 0x00;
+    P3M0 &= 0xC3;
+    P3M1 &= 0xC3;
+  } else {
+    P2M0 = 0xFF;
+    P2M1 = 0x00;
+    P3M0 |= 0x3C;
+    P3M1 &= 0xC3;
+  }
+}
+
 void main() {
   unsigned char hours_high, hours_low, minutes_high, minutes_low, seconds_mark;
   reset_io();
 
   ds1302_init();
+  stc15_dusk_init();
 
   while(1) {
     ds1302_get_time(&hours_high, &hours_low, &minutes_high, &minutes_low, &seconds_mark);
     show_time(hours_high, hours_low, minutes_high, minutes_low, seconds_mark);
     read_buttons();
+    check_low_light();
   }
 }

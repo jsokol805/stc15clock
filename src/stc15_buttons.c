37d1dea2dfd7ac3019e35dc8c580dd51f9684f5d
Jakub Sokół <beamer.kun@gmail.com>
2017-01-10 23:39:36 +0100

    Improve button scan routine
    - mode now changes by pressing both buttons
    - clock setting is now the same as in v1.0 release
    
    - fixed small issue in ds1302 support ( 12/24 was set in wrong way )

--- src/stc15_buttons.c
+++ src/stc15_buttons.c
@@ -2,13 +2,60 @@
 
 #include "ds1302.h"
 
-uint8_t _buttons[2] = {0, 0};
+#define BUTTON_ACTIVATION_TICKS 6
 
-bool stc15_button_check(uint8_t index, uint8_t value) {
-  if ( _buttons[index] != value ) {
-    _buttons[index] = value;
-    return value;
+enum BUTTON_STATE {
+  BUTTON_OFF = 0,
+  BUTTON_PRESS,
+  BUTTON_PRESSED
+};
+
+uint8_t _buttons[2] = {0, 0};
+uint8_t _buttons_state[2] = {0, 0};
+
+bool stc15_both_button_pressed() {
+  if( _buttons_state[0] == BUTTON_PRESS &&
+      _buttons_state[1] == BUTTON_PRESS ) {
+    _buttons_state[0] = _buttons_state[1] = BUTTON_PRESSED;
+    return true;
   }
   return false;
+}
+
+bool stc15_button_check(uint8_t index) {
+  if( _buttons_state[index] == BUTTON_PRESS && !_buttons[BUTTON_NUM - index - 1]) {
+    _buttons_state[index] = BUTTON_PRESSED;
+    return true;
+  }
+  return false;
+}
+
+void stc15_button_scan() {
+  uint8_t loop;
+  uint8_t values[BUTTON_NUM];
+  values[UPPER_BTN] = UPPER_PIN;
+  values[LOWER_BTN] = LOWER_PIN;
+
+  for( loop = 0; loop < BUTTON_NUM; loop++ ) {
+    switch (_buttons_state[loop]) {
+      case BUTTON_OFF:
+        _buttons[loop] += values[loop] ? -_buttons[loop] : 1;
+
+        if (_buttons[loop] > BUTTON_ACTIVATION_TICKS) {
+          _buttons_state[loop] = BUTTON_PRESS;
+        } else if (_buttons[loop] && 
+                   _buttons[loop] < _buttons[BUTTON_NUM - loop - 1]) {
+          // Sync up values to properly detect both button press
+          _buttons[loop] = _buttons[BUTTON_NUM - loop - 1];
+        }
+        break;
+      case BUTTON_PRESS:
+      case BUTTON_PRESSED:
+      default:
+        if (values[loop])
+          _buttons_state[loop] = BUTTON_OFF;
+        break;
+    }
+  }
 }
 
--- src/stc15_buttons.h
+++ src/stc15_buttons.h
@@ -10,8 +10,16 @@
 #define LOWER_BTN 1
 #define LOWER_PIN P3_0
 
+#define BUTTON_NUM 2
+
+/* check is both buttons are pressed - SHOULD BE CALLED BEFORE ANY stc15_button_check!! */
+bool stc15_both_button_pressed();
+
 /* checks button state, retuns true if state changes to high */
-bool stc15_button_check(uint8_t /* index */, uint8_t /* value */);
+bool stc15_button_check(uint8_t /* index */);
+
+/* scans current button state */
+void stc15_button_scan();
 
 #endif  // __STC15_BUTTONS_H_
 

8043ea1281a4756e1f8a7c49cd7aa4eab24488f5
Alexander Kollert <akollert@gmail.com>
2017-02-09 14:46:40 +0100

    if-else statement to if-else macro

--- src/stc15_display.c
+++ src/stc15_display.c
@@ -38,17 +38,14 @@ void _digit(uint8_t pos, uint8_t val, uint8_t dot) {
   /* Iterator for loop */
   uint8_t i;
 
-#pragma save
-#pragma disable_warning 126
   /* Reset bits P3_2..5 */
-  if (COMMON_PIN_ACTIVE_STATE) {
+#ifdef COMMON_PIN_ACTIVE_STATE
     P3 &= 0xC3;
     P3 |= 4 << (3 - pos);
-  } else {
+#else    
     P3 |= 0x3C;
     P3 &= ~(4 << (3 - pos));
-  }
-#pragma restore
+#endif
 
   /* Light up single segments to ensure even brightness */
   for(i = 0; i < 7; ++i) {
